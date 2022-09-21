6fbe28eeccbdf114bd44eb4b18adccd91f072da4
Dan Thackray <djthackray@gmail.com>
2022-09-21 14:28:18 +0100

    Initial HTML scaffolding within Select component
    Signed-off-by: Dan Thackray <djthackray@gmail.com>

--- src/Select.tsx
+++ src/Select.tsx
@@ -15,7 +15,15 @@ type SelectProps = {
 export function Select({ value, onChange, options }: SelectProps) {
   return (
     <div className={styles.container}>
-
+        <span className={styles.value}>Value</span>
+        <button className={styles["clear-btn"]}>&times;</button>
+        <div className={styles.divider}></div>
+        <div className={styles.caret}></div>
+        <ul className={styles.options}>
+            {options.map(option => (
+                <li key={option.value} className={styles.option}>{option.label}</li>
+            ))}
+        </ul>
     </div>
   )
 }
