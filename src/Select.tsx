a49feab682a73e6cda212916ee4d6df5149dcb27
Dan Thackray <djthackray@gmail.com>
2022-09-21 14:59:17 +0100

    Field updates when user selects a different option
    Signed-off-by: Dan Thackray <djthackray@gmail.com>

--- src/Select.tsx
+++ src/Select.tsx
@@ -15,6 +15,14 @@ type SelectProps = {
 export function Select({ value, onChange, options }: SelectProps) {
     const [isOpen, setIsOpen] = useState(false)
 
+    function clearOptions() {
+        onChange(undefined)
+    }
+
+    function selectOption(option: SelectOption) {
+        onChange(option)
+    }
+
     return (
         <div 
             onBlur={() => setIsOpen(false)}
@@ -23,12 +31,19 @@ export function Select({ value, onChange, options }: SelectProps) {
             className={styles.container}
         >
             <span className={styles.value}>{value?.label}</span>
-            <button className={styles["clear-btn"]}>&times;</button>
+            <button onClick={e => {
+                e.stopPropagation()
+                clearOptions()
+            }} className={styles["clear-btn"]}>&times;</button>
             <div className={styles.divider}></div>
             <div className={styles.caret}></div>
             <ul className={`${styles.options} ${isOpen ? styles.show : ""}`}>
                 {options.map(option => (
-                    <li key={option.label} className={styles.option}>{option.label}</li>
+                    <li onClick={e => {
+                        e.stopPropagation()
+                        selectOption(option)
+                        setIsOpen(false)
+                    }} key={option.label} className={styles.option}>{option.label}</li>
                 ))}
             </ul>
         </div>
